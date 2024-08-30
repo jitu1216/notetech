e899c3e6fb50e1554c8dde5a65796e8bf7489823
ankitrathore31 <ankitrathore3110@gmail.com>
2024-08-30 16:57:59 +0530

    Dashboard Update

--- resources/views/studentDashboard/student-home.blade.php
+++ resources/views/studentDashboard/student-home.blade.php
@@ -2,9 +2,314 @@
 @section('content')
     <div class="page-wrapper">
         <div class="content container-fluid">
-            <h1>Hi</h1>
+  
+            <div class="row">
+                <div class="db-info">
+                    <figcaption class="blockquote-footer">
+                         Fees Status
+                      </figcaption>
+                </div>
+                <div class="col-xl-4 col-sm-4 col-12 d-flex">
+                    <div class="card bg-comman w-100"
+                        style="background-image: linear-gradient(to right, rgb(15, 240, 27), rgb(27, 116, 0));">
+                        <div class="card-body">
+                            <div class="db-widgets d-flex justify-content-between align-items-center">
+                                <div class="db-info">
+                                    <h6 style="color: yellow;">Total Fees</h6>
+                                    <h3 style="color: rgb(255, 255, 255);">{{-- Custom::getApprovedStudent() --}}</h3>
+                                </div>
+                                <div>
+                                    <i class="fas fa-dollar-sign" style="font-size: 50px; color:white;"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-sm-4 col-12 d-flex">
+                    <div class="card bg-comman w-100"
+                        style="background-image: linear-gradient(to right, rgb(216, 195, 6), rgb(172, 154, 0));">
+                        <div class="card-body">
+                            <div class="db-widgets d-flex justify-content-between align-items-center">
+                                <div class="db-info">
+                                    <h6 style="color: rgb(255, 255, 10);">Total Deposit Fees</h6>
+                                    <h3 style="color: rgb(255, 255, 255);">{{-- Custom::getpendingStudent() --}}</h3>
+                                </div>
+                                <div>
+                                    <i class="fas fa-rupee-sign" style="font-size: 50px; color:white;"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-sm-4 col-12 d-flex">
+                    <div class="card bg-comman w-100"
+                        style="background-image: linear-gradient(to right, rgb(253, 67, 54), rgb(185, 6, 0));">
+                        <div class="card-body">
+                            <div class="db-widgets d-flex justify-content-between align-items-center">
+                                <div class="db-info">
+                                    <h6 style="color: yellow;">Total Pending Fees</h6>
+                                    <h3 style="color: rgb(255, 255, 255);">{{-- Custom::getrejectedStudent() --}}</h3>
+                                </div>
+                                <div>
+                                    <i class="fas fa-rupee-sign" style="font-size: 50px; color:white;"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="db-info">
+                    <figcaption class="blockquote-footer">
+                        Back Fees Status
+                      </figcaption>
+                </div>
+                <div class="col-xl-4 col-sm-4 col-12 d-flex">
+                    <div class="card bg-comman w-100"
+                        style="background-image: linear-gradient(to right, rgb(15, 240, 27), rgb(27, 116, 0));">
+                        <div class="card-body">
+                            <div class="db-widgets d-flex justify-content-between align-items-center">
+                                <div class="db-info">
+                                    <h6 style="color: yellow;">Total Back Fees</h6>
+                                    <h3 style="color: rgb(255, 255, 255);">{{-- Custom::getApprovedStudent() --}}</h3>
+                                </div>
+                                <div>
+                                    <i class="fas fa-rupee-sign" style="font-size: 50px; color:white;"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-sm-4 col-12 d-flex">
+                    <div class="card bg-comman w-100"
+                        style="background-image: linear-gradient(to right, rgb(216, 195, 6), rgb(172, 154, 0));">
+                        <div class="card-body">
+                            <div class="db-widgets d-flex justify-content-between align-items-center">
+                                <div class="db-info">
+                                    <h6 style="color: rgb(255, 255, 10);">Total Back Fees Deposit</h6>
+                                    <h3 style="color: rgb(255, 255, 255);">{{-- Custom::getpendingStudent() --}}</h3>
+                                </div>
+                                <div>
+                                    <i class="fas fa-rupee-sign" style="font-size: 50px; color:white;"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-sm-4 col-12 d-flex">
+                    <div class="card bg-comman w-100"
+                        style="background-image: linear-gradient(to right,  rgb(253, 67, 54), rgb(185, 6, 0));">
+                        <div class="card-body">
+                            <div class="db-widgets d-flex justify-content-between align-items-center">
+                                <div class="db-info">
+                                    <h6 style="color: rgb(255, 255, 10);">Total Back Fees Pending</h6>
+                                    <h3 style="color: rgb(255, 255, 255);">{{-- Custom::getpendingStudent() --}}</h3>
+                                </div>
+                                <div>
+                                    <i class="fas fa-rupee-sign" style="font-size: 50px; color:white;"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="db-info">
+                    <figcaption class="blockquote-footer">
+                        Attendance Status
+                      </figcaption>
+                </div>
+                <div class="col-xl-4 col-sm-4 col-12 d-flex">
+                    <div class="card bg-comman w-100"
+                        style="background-image: linear-gradient(to right, rgb(54, 253, 203), rgb(0, 132, 185));">
+                        <div class="card-body">
+                            <div class="db-widgets d-flex justify-content-between align-items-center">
+                                <div class="db-info">
+                                    <h6 style="color: yellow;">Total Attendance</h6>
+                                    <h3 style="color: rgb(255, 255, 255);">{{-- Custom::getStaff() --}}</h3>
+                                </div>
+                                <div>
+                                    <i class="fas fa-user-graduate" style="font-size: 50px; color:white;"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-sm-4 col-12 d-flex">
+                    <div class="card bg-comman w-100"
+                        style="background-image: linear-gradient(to right, rgb(253, 54, 193), rgb(185, 0, 92));">
+                        <div class="card-body">
+                            <div class="db-widgets d-flex justify-content-between align-items-center">
+                                <div class="db-info">
+                                    <h6 style="color: yellow;">Total Present</h6>
+                                    
+                                        <h3 style="color: rgb(255, 255, 255);">{{-- Custom::classtotalfees() --}}</h3>
+                                   
+                                        <h3 style="color: rgb(255, 255, 255);">{{-- Custom::schoototalfees() --}}</h3>
+                                    
+                                </div>
+                                <div>
+                                    <i class="fas fa-user-graduate" style="font-size: 50px; color:white;"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-sm-4 col-12 d-flex">
+                    <div class="card bg-comman w-100"
+                        style="background-image: linear-gradient(to right, rgb(96, 149, 248), rgb(3, 59, 163));">
+                        <div class="card-body">
+                            <div class="db-widgets d-flex justify-content-between align-items-center">
+                                <div class="db-info">
+                                    <h6 style="color: yellow;">Total Absent</h6>
+                                    <h3 style="color: rgb(255, 255, 255);">{{-- Custom::todayfees() --}}</h3>
+                                </div>
+                                <div>
+                                    <i class="fas fa-user-graduate" style="font-size: 50px; color:white;"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             
-           
-        </div>
+                <div class="col-xl-4 col-sm-4 col-12 d-flex">
+                    <div class="card bg-comman w-100"
+                        style="background-image: linear-gradient(to right, rgb(121, 39, 252), rgb(72, 3, 163));">
+                        <div class="card-body">
+                            <div class="db-widgets d-flex justify-content-between align-items-center">
+                                <div class="db-info">
+                                    <h6 style="color: yellow;">Total Pending Attendance</h6>
+                                    
+                                        <h3 style="color: rgb(255, 255, 255);">{{-- Custom::classtotalpaidfees()--}}</h3>
+                                    
+                                        <h3 style="color: rgb(255, 255, 255);">{{--Custom::schoototalpaidfees() --}}</h3>
+                                    
+                                </div>
+                                <div>
+                                    <i class="fas fa-user-graduate" style="font-size: 50px; color:white;"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>             
+            </div>
+            <div class="row">
+                <div class="db-info">
+                    <figcaption class="blockquote-footer">
+                        More Information
+                      </figcaption>
+                </div>
+                <div class="col-xl-4 col-sm-4 col-12 d-flex">
+                    <div class="card bg-comman w-100"
+                        style="background-image: linear-gradient(to right, rgb(54, 253, 203), rgb(0, 132, 185));">
+                        <div class="card-body">
+                            <div class="db-widgets d-flex justify-content-between align-items-center">
+                                <div class="db-info">
+                                    <h6 style="color: yellow;">Test Marks</h6>
+                                    <h3 style="color: rgb(255, 255, 255);">{{-- Custom::getStaff() --}}</h3>
+                                </div>
+                                <div>
+                                    <i class="fas fa-table" style="font-size: 50px; color:white;"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-sm-4 col-12 d-flex">
+                    <div class="card bg-comman w-100"
+                        style="background-image: linear-gradient(to right, rgb(253, 54, 193), rgb(185, 0, 92));">
+                        <div class="card-body">
+                            <div class="db-widgets d-flex justify-content-between align-items-center">
+                                <div class="db-info">
+                                    <h6 style="color: yellow;">Class Teacher</h6>
+                                    
+                                        <h3 style="color: rgb(255, 255, 255);">{{-- Custom::classtotalfees() --}}</h3>
+                                   
+                                        <h3 style="color: rgb(255, 255, 255);">{{-- Custom::schoototalfees() --}}</h3>
+                                    
+                                </div>
+                                <div>
+                                    <i class="fas fa-user-graduate" style="font-size: 50px; color:white;"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-sm-4 col-12 d-flex">
+                    <div class="card bg-comman w-100"
+                        style="background-image: linear-gradient(to right, rgb(96, 149, 248), rgb(3, 59, 163));">
+                        <div class="card-body">
+                            <div class="db-widgets d-flex justify-content-between align-items-center">
+                                <div class="db-info">
+                                    <h6 style="color: yellow;">Staff</h6>
+                                    <h3 style="color: rgb(255, 255, 255);">{{-- Custom::todayfees() --}}</h3>
+                                </div>
+                                <div>
+                                    <i class="fas fa-user-graduate" style="font-size: 50px; color:white;"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+                <div class="col-xl-4 col-sm-4 col-12 d-flex">
+                    <div class="card bg-comman w-100"
+                        style="background-image: linear-gradient(to right, rgb(54, 253, 203), rgb(0, 132, 185));">
+                        <div class="card-body">
+                            <div class="db-widgets d-flex justify-content-between align-items-center">
+                                <div class="db-info">
+                                    <h6 style="color: yellow;">Intitute Rules</h6>
+                                    
+                                        <h3 style="color: rgb(255, 255, 255);">{{-- Custom::classtotalpaidfees()--}}</h3>
+                                    
+                                        <h3 style="color: rgb(255, 255, 255);">{{--Custom::schoototalpaidfees() --}}</h3>
+                                    
+                                </div>
+                                <div>
+                                    <i class="fas fa-book" style="font-size: 50px; color:white;"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+                <div class="col-xl-4 col-sm-4 col-12 d-flex">
+                    <div class="card bg-comman w-100"
+                        style="background-image: linear-gradient(to right, rgb(253, 54, 193), rgb(185, 0, 92));">
+                        <div class="card-body">
+                            <div class="db-widgets d-flex justify-content-between align-items-center">
+                                <div class="db-info">
+                                    <h6 style="color: yellow;">Transport Fees Info</h6>
+                                    
+                                        <h3 style="color: rgb(255, 255, 255);">{{-- Custom::classtotalfees() --}}</h3>
+                                   
+                                        <h3 style="color: rgb(255, 255, 255);">{{-- Custom::schoototalfees() --}}</h3>
+                                    
+                                </div>
+                                <div>
+                                    <i class="fas fa-dollar-sign" style="font-size: 50px; color:white;"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-sm-4 col-12 d-flex">
+                    <div class="card bg-comman w-100"
+                        style="background-image: linear-gradient(to right, rgb(96, 149, 248), rgb(3, 59, 163));">
+                        <div class="card-body">
+                            <div class="db-widgets d-flex justify-content-between align-items-center">
+                                <div class="db-info">
+                                    <h6 style="color: yellow;">Hostel Fees Info</h6>
+                                    <h3 style="color: rgb(255, 255, 255);">{{-- Custom::todayfees() --}}</h3>
+                                </div>
+                                <div>
+                                    <i class="fas fa-dollar-sign" style="font-size: 50px; color:white;"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>            
+            </div>
+
+         </div>
     </div>
 @endsection
